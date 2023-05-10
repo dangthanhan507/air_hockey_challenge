11f3a3f7d9a56d29a31fa309566040aeb578050e
Justin Yue <justinyue1643@gmail.com>
2023-05-09 19:00:59 -0700

    increasing size of NN improves performance of BC

--- BC_defense_train.py
+++ BC_defense_train.py
@@ -5,6 +5,7 @@ from air_hockey_challenge.framework.air_hockey_challenge_wrapper import AirHocke
 import gym
 from imitation.algorithms import bc
 import torch
+from util_networks import AirHockeyACPolicy
 
 with open("defend_training_data.pkl", "rb") as f:
     training_data = pickle.load(f)
@@ -30,15 +31,17 @@ high = np.hstack((jnt_range[:,1][:,np.newaxis], vel_range[:,1][:,np.newaxis])).T
 bc_action_space = gym.spaces.Box(low=low, high=high, shape=(2,3))
 transitions = Transitions(obs=obs, acts=actions, infos=info, next_obs=next_obs, dones=dones)
 
+custom_policy = AirHockeyACPolicy(bc_obs_space, bc_action_space) 
+
 rng = np.random.default_rng()
 bc_trainer = bc.BC(
     observation_space=bc_obs_space,
     action_space=bc_action_space,
     demonstrations=transitions,
     rng=rng,
+    batch_size=256,
+    policy=custom_policy
 )
 
-bc_trainer.train(n_epochs=1)
-bc_trainer.save_policy("defense_bc_policy")
-torch.save(bc_trainer.policy.state_dict(), "defense_bc_weights.pt")
-
+bc_trainer.train(n_epochs=200)
+bc_trainer.save_policy("defense_bc_policy2_1024")
