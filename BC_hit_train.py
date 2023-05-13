d20dcbdb27e1104bacd4013663078ff0f4fc7655
An Dang <dangthanhan507@gmail.com>
2023-05-12 18:07:25 -0700

    modify observation space

--- BC_hit_train.py
+++ BC_hit_train.py
@@ -9,6 +9,11 @@ that we have a checkpoint of. Otherwise, a policy is trained from scratch.
 When a policy is trained from a checkpoint, `bc.reconstruct_policy(<file_path>)` is used.
 It's much easier than saving and loading the policy's state_dict() since by saving just 
 the policy, then reconstructing it will return the entire policy.
+
+Argument #2
+=============
+    -> (-exclude_robot_obs): option to train without
+
 """
 import argparse
 import pickle
@@ -24,11 +29,17 @@ from air_hockey_challenge.framework.air_hockey_challenge_wrapper import \
     AirHockeyChallengeWrapper
 from util_networks import AirHockeyACPolicy
 
+from imitation.util import logger as imit_logger
+
+#ARGUMENTS
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--policy", type=str, default="")
+parser.add_argument("-e", "--exclude_robot_obs"  ,type=bool, default=False)
 args = parser.parse_args()
 
-with open("hit_training_data.pkl", "rb") as f:
+
+#LOAD TRAINING DATA
+with open("training_data.pkl", "rb") as f:
     training_data = pickle.load(f)
     obs = training_data["obs"]
     actions = training_data["actions"].reshape(training_data["actions"].shape[0],-1)
@@ -36,12 +47,23 @@ with open("hit_training_data.pkl", "rb") as f:
     dones = training_data["dones"]
     info = training_data["info"]
 
+
+#SETUP MDP
 mdp = AirHockeyChallengeWrapper(env="3dof-hit")
 obs_space = mdp.info.observation_space
+
+'''
+    BIG NOTES:
+        -> wtf is this shit
+        -> observation space make no sense
+'''
+obs_space.low[0:3] = np.array([-10,-10,-np.pi])
+obs_space.high[0:3] = np.array([10,10,np.pi])
 bc_obs_space = gym.spaces.Box(low=obs_space.low, high=obs_space.high, shape=obs_space.shape)
 
 action_space = mdp.info.action_space
 
+#SETUP EXPERT SUPERVISED DATA
 jnt_range = mdp.base_env.env_info['robot']['robot_model'].jnt_range
 vel_range = mdp.base_env.env_info['robot']['joint_vel_limit'].T * 0.95
 
@@ -51,25 +73,32 @@ high = np.hstack((jnt_range[:,1][:,np.newaxis], vel_range[:,1][:,np.newaxis])).T
 bc_action_space = gym.spaces.Box(low=low, high=high, shape=(2,3))
 transitions = Transitions(obs=obs, acts=actions, infos=info, next_obs=next_obs, dones=dones)
 
+#SETUP POLICY NETWORK FOR TRAINING
 if len(args.policy) != 0:
     custom_policy = bc.reconstruct_policy(args.policy)
 else:
-    custom_policy = AirHockeyACPolicy(bc_obs_space, bc_action_space) 
+    custom_policy = AirHockeyACPolicy(bc_obs_space, bc_action_space)
 
+
+#SETUP IMITATION LEARNING
 rng = np.random.default_rng()
 bc_trainer = bc.BC(
     observation_space=bc_obs_space,
     action_space=bc_action_space,
     demonstrations=transitions,
     rng=rng,
-    batch_size=256,
-    policy=custom_policy
+    batch_size=100,
+    policy=custom_policy.cuda(),
+    custom_logger=imit_logger.configure('bc_hit_tensorboard/'),
+    device='cuda',
 )
 
-bc_trainer.train(n_epochs=200)
+#TRAIN
+bc_trainer.train(n_epochs=1000,log_interval=10)
 
+#SAVE MODEL
 policy_ckpt_dir = Path("./policy_ckpts/")
 if not policy_ckpt_dir.exists():
     policy_ckpt_dir.mkdir(parents=True, exist_ok=False)
 n_files = len([_ for _ in policy_ckpt_dir.iterdir()])
-bc_trainer.save_policy(f"defense_bc_policy_{n_files + 1}.pt")
+bc_trainer.save_policy(f"policy_ckpts/defense_bc_policy_{n_files + 1}.pt")
